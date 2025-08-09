f991af100358473b0cdb920628fd853186d45353
wlvr666 <wlvr3825@gmail.com>
2025-08-08 23:55:43 -0500

    Update app.js

--- webxr/app.js
+++ webxr/app.js
@@ -1,1 +1,5 @@
-
+if ('xr' in navigator) {
+  navigator.xr.isSessionSupported('immersive-ar').then(supported => {
+    console.log(supported ? "WebXR supported!" : "WebXR not supported.");
+  });
+}
