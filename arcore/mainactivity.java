51bff90090b8d931ecde4e157ab464c97330b9df
wlvr666 <wlvr3825@gmail.com>
2025-08-08 23:56:38 -0500

    Update mainactivity.java

--- arcore/mainactivity.java
+++ arcore/mainactivity.java
@@ -1,1 +1,12 @@
+package com.example.arvrphone;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ARCore setup code will go here
+    }
+}
