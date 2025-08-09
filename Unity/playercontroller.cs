aaeca84628c4f9f9d6d73d2e1b62607646a906cc
wlvr666 <wlvr3825@gmail.com>
2025-08-08 23:53:42 -0500

    Update playercontroller.cs

--- Unity/playercontroller.cs
+++ Unity/playercontroller.cs
@@ -1,1 +1,11 @@
+using UnityEngine;
 
+public class PlayerController : MonoBehaviour {
+    public float speed = 5f;
+
+    void Update() {
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(moveX, 0, moveZ);
+    }
+}
